699a0305a41658c24de393da57e72a26e411fb06
Oshayer <oshayer.siddique2001@gmail.com>
2023-10-29 00:25:05 +0600

    sixth

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 // Create an Express application
 const app = express();
-const port = 3000; // You can choose any port you like
+const port = 3002; // You can choose any port you like
 
 // Define a route and its handler
 app.get('/', (req, res) => {
